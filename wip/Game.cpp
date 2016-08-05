77ef5b00dbcdaf5ea2531ab1fe4560c59fc05e61
Ackou Jens <ackoujens@gmail.com>
2016-08-05 20:56:28 +0200

    Initial commit using static GLFW source

---
+++ wip/Game.cpp
@@ -0,0 +1,12 @@
+//
+//  Game.cpp
+//  glfw-cmake-boilerplate
+//
+//  Created by Ackou Jens on 05/08/2016.
+//
+//
+
+#include "Game.hpp"
+
+Game::Game()  {}
+Game::~Game() {}
---
+++ wip/Game.hpp
@@ -0,0 +1,25 @@
+//
+
+//  Game.hpp
+//  glfw-cmake-boilerplate
+//
+//  Created by Ackou Jens on 05/08/2016.
+//
+//
+
+#ifndef Game_hpp
+#define Game_hpp
+
+class Game {
+private:
+
+public:
+
+  Game();
+  ~Game();
+
+protected:
+};
+
+
+#endif /* Game_hpp */
