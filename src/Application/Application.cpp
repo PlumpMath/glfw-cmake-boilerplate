ceca637278431de0f91d53e68a47ef7d2ab5e01f
Ackou Jens <ackoujens@gmail.com>
2016-08-14 14:57:38 +0200

    Separate the application code into cpp and hop files.
    Putting everything in 1 .hpp file was an idea by OpenGL SuperBible.
    Seriously, something messy as that is NOT readable anymore.

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -7,10 +7,58 @@
 //
 
 #include "Application.hpp"
-#include <iostream>
-#include <GLFW/glfw3.h>
 
-int Application::initialize() {
+Application::Application() {
+    window = NULL;
+}
+
+Application::~Application() {}
+
+void Application::run(Application *app) {
+    using namespace std;
+        cout << "Running Application ..." << endl;
+    
+        // GLFW Init
+        if (!glfwInit()) {
+            cout << "GLFW initialization failed !" << endl;
+        } else {
+            cout << "GLFW initialized" << endl;
+        }
+        
+        // Enabled OSX to use a more advanced OpenGL Version
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+        // Create Window
+        this->window = glfwCreateWindow(640,
+                                        480,
+                                        "My Application",
+                                        NULL,
+                                        NULL);
+        if (!window) {
+            cout << "Window or OpenGL context creation failed !" << endl;
+        } else {
+            cout << "Window or OpenGL context created" << endl;
+        }
+        glfwMakeContextCurrent(this->window);
+    
+        // Game Loop
+        while (true) {
+            render(glfwGetTime());
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    
+        // Destruct
+        glfwDestroyWindow(window);
+        glfwTerminate();
+}
+
+void Application::render(double currentTime){}
+
+/*int Application::initialize() {
   if (!glfwInit())
   {
     std::cout << "GLFW initialization failed !" << std::endl;
@@ -72,4 +120,4 @@ Application::~Application() {
 
   glfwDestroyWindow(this->window);
   glfwTerminate();
-}
+}*/
--- src/Application/Application.hpp
+++ src/Application/Application.hpp
@@ -9,58 +9,19 @@
 #ifndef Application_hpp
 #define Application_hpp
 
-// #include <string>
 #include <iostream>
 #include <GLFW/glfw3.h>
 
 class Application {
 public:
+    GLFWwindow *window;
+    
+    Application();
+    virtual ~Application();
 
-  Application() {}
+    virtual void run(Application *app);
 
-  virtual ~Application() {}
-
-  GLFWwindow *window;
-
-
-  virtual void run(Application *app) {
-    std::cout << "Running Application ..." << std::endl;
-
-    if (!glfwInit()) {
-      std::cout << "GLFW initialization failed !" << std::endl;
-    } else {
-      std::cout << "GLFW initialized" << std::endl;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    this->window = glfwCreateWindow(640,
-                                    480,
-                                    "My Application",
-                                    NULL,
-                                    NULL);
-
-    if (!window) {
-      std::cout << "Window or OpenGL context creation failed !" << std::endl;
-    } else {
-      std::cout << "Window or OpenGL context created" << std::endl;
-    }
-
-    glfwMakeContextCurrent(this->window);
-
-    while (true) {
-      render(glfwGetTime());
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-  }
-
-  virtual void render(double currentTime) {}
+    virtual void render(double currentTime);
 };
 
 #define DECLARE_MAIN(a)                 \
