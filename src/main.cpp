f42dea600f378cdc54de901fc98ee7e347384b29
Ackou Jens <ackoujens@gmail.com>
2016-08-14 14:56:26 +0200

    Cleanup of commented out code

--- src/main.cpp
+++ src/main.cpp
@@ -9,24 +9,11 @@
 class my_application : public Application {
 public:
 
-  // Overridden Render Function
+  // Override Virtual Render Function
   void render(double currentTime) {
-    // FIXME Undeclared identifier
-    // Clear the window with a red color
     static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-
     glClearBufferfv(GL_COLOR, 0, red);
-
-    /*
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(this->window);
-     */
   }
 };
 
 DECLARE_MAIN(my_application);
-
-// int main() {
-//  Application application("glfw-cmake-boilerplate", 640, 480);
-// }
